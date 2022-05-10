c9231b3fd4493cdadfe0584ce969e3943890d050
İbrahim Gedik <ibrahimgediktc@gmail.com>
2022-05-10 19:04:57 +0300

    create style folder structure

--- components/Layout/Header/Header.tsx
+++ components/Layout/Header/Header.tsx
@@ -25,33 +25,30 @@ export default function Header() {
             <Navbar.Brand href="#home">E-Commerce</Navbar.Brand>
           </div>
           <Nav className="gap-3 gap-lg-1 flex-row order-lg-2">
-            <Nav.Link className="d-none d-md-block search-toggler" tabIndex={1} href="#">
+            <Nav.Link className="d-none d-md-block" href="#1">
               <FontAwesomeIcon size="lg" icon={faSearch} />
-              <div className="search-wrapper ">
-                <SearchBar/>
-              </div>
             </Nav.Link>
-            <Nav.Link href="#">
+            <Nav.Link href="#2">
               <FontAwesomeIcon size="lg" icon={faHeart} />
             </Nav.Link>
-            <Nav.Link className="icon-container" href="#">
+            <Nav.Link className="icon-container" href="#4">
               <FontAwesomeIcon size="lg" icon={faShoppingBasket} />
               <Badge className="c-badge" pill bg="danger">
                 3
               </Badge>
             </Nav.Link>
-            <Nav.Link href="#">
+            <Nav.Link href="#3">
               <FontAwesomeIcon size="lg" icon={faUser} />
             </Nav.Link>
           </Nav>
           <Nav className="mx-auto gap-0 gap-lg-5 d-none d-lg-flex">
             <Nav.Link href="#features">Women</Nav.Link>
             <Nav.Link href="#pricing">Men</Nav.Link>
-            <Nav.Link href="#pricing">Baby</Nav.Link>
+            <Nav.Link href="#baby">Baby</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
-      <div className="container py-2 d-md-none"><SearchBar/></div>
+
       <OffCanvasMenu show={show} handleClose={() => setShow(false)} />
     </header>
   );
